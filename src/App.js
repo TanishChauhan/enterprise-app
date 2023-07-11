59a1af093abe49f306fc928e90a1de1211e6af28
Tanish <tanish1472@gmail.com>
2023-07-11 12:49:09 +0530

    Contact page added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import './styles/App.scss';
-import './styles/home.scss';
-import './styles/footer.scss'
+//components
 import Header from  './components/Header';
 import Footer from "./components/Footer";
 import Home from './components/Home'
+import Contact from "./components/Contact";
+//css
+import './styles/App.scss';
+import './styles/home.scss';
+import './styles/footer.scss';
+import './styles/contact.scss';
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
         <Route path = "/" element={<Home/>}/>
+        <Route path = "/contact" element={<Contact/>}/>
       </Routes>
       <Footer/>
     </Router>
