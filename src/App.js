9510e205db61aa18c240360495ce3662d13bfd7c
Tanish <tanish1472@gmail.com>
2023-07-11 12:29:15 +0530

    Home page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import './styles/App.scss';
+import './styles/home.scss';
+import './styles/footer.scss'
 import Header from  './components/Header';
+import Footer from "./components/Footer";
+import Home from './components/Home'
 function App() {
   return (
     <Router>
       <Header/>
       <Routes>
-        <Route/>
+        <Route path = "/" element={<Home/>}/>
       </Routes>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>GoodWill</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <p  >
+            Youtube
+          </p>
+          <p>
+            Instagram
+          </p>
+          <a href="https://github.com/TanishChauhan" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
